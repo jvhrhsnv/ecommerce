5d509497ccd39b413b82a4bd056ef90d655b2c77
Javohir Hasanov <javohirhasanov3303@gmail.com>
2022-03-26 04:43:50 +0500

    add new change big photo codes and product iterator

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 var counterPlus = document.querySelector('.counter-plus'),
     counterMinus = document.querySelector('.counter-minus'),
     inputValue = document.querySelector('#input-value'),
-    cartNumber = document.querySelector('cart-number');
+    cartNumber = document.querySelector('cart-number-old');
 
-counterPlus.addEventListener('click', () => {
-    for (var i = 0; i < 100; i++) {
-        var newI = i;
-        inputValue.value = newI;
-        i++;  
-    }  
-});
+function numberPlus() {
+    var i = 0;
+    counterPlus.addEventListener('click', () => {
+       i++;
+       inputValue.value = i;
+       cartNumber.value = inputValue.value;
+    });
+}
+function numberMinus() {
+    counterMinus.addEventListener('click', () => {
+       if (inputValue.value > 0) {
+        var newMinus = inputValue.value - 1;
+        inputValue.value = newMinus;
+        cartNumber.value = inputValue.value;
+       }
+      
+    });
+}
+numberPlus();
+numberMinus();
 
-counterMinus.addEventListener('click', () => {
-    // var = 
-    if (inputValue > 0) {
-        
-    }
-});
 
 
 // popup
@@ -37,31 +44,22 @@ opacity.addEventListener('click', () => {
 },2000);
 
 
-// change big photo
-
-var tapImg1 = document.querySelector('.tap-img1'),
-    tapImg2 = document.querySelector('.tap-img2'),
-    tapImg3 = document.querySelector('.tap-img3'),
-    tapImg4 = document.querySelector('.tap-img4'),
-    changeImg = document.querySelector('.change-img');
-
-tapImg1.addEventListener('click', () => {   
-   changeImg.src = 'images/image-product-1.jpg';
+// Change bi photo
+var sectionLeft = document.querySelector('.section__left');
+sectionLeft.addEventListener('click', (e) => {
+var tapImg1 = document.querySelector('.tap-img1');
+var tapImg2 = document.querySelector('.tap-img2');
+var tapImg3 = document.querySelector('.tap-img3');
+var tapImg4 = document.querySelector('.tap-img4');
+var changeImg = document.querySelector('.change-img');
+    var classListName = e.target.classList;
+   if (classListName[0] === "tap-img1") {      
+       changeImg.setAttribute('src', 'images/image-product-1.jpg');
+   }else if (classListName[0] === "tap-img2") {
+       changeImg.setAttribute('src', 'images/image-product-2.jpg');
+   }else if (classListName[0] === "tap-img3") {
+       changeImg.setAttribute('src', 'images/image-product-3.jpg');
+   }else {
+        changeImg.setAttribute('src', 'images/image-product-4.jpg');
+   }
 });
-tapImg2.addEventListener('click', () => {   
-    changeImg.src = 'images/image-product-2.jpg';
-    tapImg2.classList.toggle('yellow-border');
- });
- tapImg3.addEventListener('click', () => {   
-    changeImg.src = 'images/image-product-3.jpg';
- });
- tapImg4.addEventListener('click', () => {   
-    changeImg.src = 'images/image-product-4.jpg';
- });
-
-
-// cartga raqam qo'shish
-
-function plusMinus() {
-    
-}
